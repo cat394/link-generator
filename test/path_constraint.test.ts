f535fcf9287911e263fdd89512621e39e57c139e
cat394 <keyring1231@gmail.com>
2024-09-10 16:23:49 +0900

    refactor: queries rename query

--- test/path_constraint.test.ts
+++ test/path_constraint.test.ts
@@ -1,171 +1,171 @@
 import { assertEquals } from "@std/assert/equals";
 import { assertType, type IsExact } from "@std/testing/types";
 import {
-  create_link_generator,
-  type ExtractRouteData,
-  type FlatRoutes,
-  flatten_route_config,
-  type RouteConfig,
+	create_link_generator,
+	type ExtractRouteData,
+	type FlatRoutes,
+	flatten_route_config,
+	type RouteConfig,
 } from "../src/mod.ts";
 
 const route_config = {
-  string_param: {
-    path: "/:param<string>",
-  },
-  number_param: {
-    path: "/:param<number>",
-  },
-  boolean_param: {
-    path: "/:param<boolean>",
-  },
-  string_query: {
-    path: "/?key<string>",
-  },
-  number_query: {
-    path: "/?key<number>",
-  },
-  boolean_query: {
-    path: "/?key<boolean>",
-  },
-  strings_union_param: {
-    path: "/:param<(a|b|c)>",
-  },
-  numbers_union_param: {
-    path: "/:param<(1|2|3)>",
-  },
-  mix_union_param: {
-    path: "/:param<(a|1|true)>",
-  },
-  strings_union_query: {
-    path: "/?key<(a|b|c)>",
-  },
-  numbers_union_query: {
-    path: "/?key<(1|2|3)>",
-  },
-  mix_union_query: {
-    path: "/?key<(a|1|true)>",
-  },
+	string_param: {
+		path: "/:param<string>",
+	},
+	number_param: {
+		path: "/:param<number>",
+	},
+	boolean_param: {
+		path: "/:param<boolean>",
+	},
+	string_query: {
+		path: "/?key<string>",
+	},
+	number_query: {
+		path: "/?key<number>",
+	},
+	boolean_query: {
+		path: "/?key<boolean>",
+	},
+	strings_union_param: {
+		path: "/:param<(a|b|c)>",
+	},
+	numbers_union_param: {
+		path: "/:param<(1|2|3)>",
+	},
+	mix_union_param: {
+		path: "/:param<(a|1|true)>",
+	},
+	strings_union_query: {
+		path: "/?key<(a|b|c)>",
+	},
+	numbers_union_query: {
+		path: "/?key<(1|2|3)>",
+	},
+	mix_union_query: {
+		path: "/?key<(a|1|true)>",
+	},
 } as const satisfies RouteConfig;
 
 const flat_route_config = flatten_route_config(route_config);
 
 Deno.test("FlatRoutes type", () => {
-  type ExpectedFlatRoutes = {
-    string_param: "/:param<string>";
-    number_param: "/:param<number>";
-    boolean_param: "/:param<boolean>";
-    string_query: "/?key<string>";
-    number_query: "/?key<number>";
-    boolean_query: "/?key<boolean>";
-    strings_union_param: "/:param<(a|b|c)>";
-    numbers_union_param: "/:param<(1|2|3)>";
-    mix_union_param: "/:param<(a|1|true)>";
-    strings_union_query: "/?key<(a|b|c)>";
-    numbers_union_query: "/?key<(1|2|3)>";
-    mix_union_query: "/?key<(a|1|true)>";
-  };
+	type ExpectedFlatRoutes = {
+		string_param: "/:param<string>";
+		number_param: "/:param<number>";
+		boolean_param: "/:param<boolean>";
+		string_query: "/?key<string>";
+		number_query: "/?key<number>";
+		boolean_query: "/?key<boolean>";
+		strings_union_param: "/:param<(a|b|c)>";
+		numbers_union_param: "/:param<(1|2|3)>";
+		mix_union_param: "/:param<(a|1|true)>";
+		strings_union_query: "/?key<(a|b|c)>";
+		numbers_union_query: "/?key<(1|2|3)>";
+		mix_union_query: "/?key<(a|1|true)>";
+	};
 
-  assertType<IsExact<FlatRoutes<typeof route_config>, ExpectedFlatRoutes>>(
-    true,
-  );
+	assertType<IsExact<FlatRoutes<typeof route_config>, ExpectedFlatRoutes>>(
+		true
+	);
 });
 
 Deno.test("ExtractRouteData type", () => {
-  type ExpectedExtractRouteData = {
-    string_param: {
-      path: "/:param";
-      params: Record<"param", string>;
-      queries: never;
-    };
-    number_param: {
-      path: "/:param";
-      params: Record<"param", number>;
-      queries: never;
-    };
-    boolean_param: {
-      path: "/:param";
-      params: Record<"param", boolean>;
-      queries: never;
-    };
-    string_query: {
-      path: "/";
-      params: never;
-      queries: Partial<Record<"key", string>>;
-    };
-    number_query: {
-      path: "/";
-      params: never;
-      queries: Partial<Record<"key", number>>;
-    };
-    boolean_query: {
-      path: "/";
-      params: never;
-      queries: Partial<Record<"key", boolean>>;
-    };
-    strings_union_param: {
-      path: "/:param";
-      params: Record<"param", "a" | "b" | "c">;
-      queries: never;
-    };
-    numbers_union_param: {
-      path: "/:param";
-      params: Record<"param", 1 | 2 | 3>;
-      queries: never;
-    };
-    mix_union_param: {
-      path: "/:param";
-      params: Record<"param", "a" | 1 | true>;
-      queries: never;
-    };
-    strings_union_query: {
-      path: "/";
-      params: never;
-      queries: Partial<Record<"key", "a" | "b" | "c">>;
-    };
-    numbers_union_query: {
-      path: "/";
-      params: never;
-      queries: Partial<Record<"key", 1 | 2 | 3>>;
-    };
-    mix_union_query: {
-      path: "/";
-      params: never;
-      queries: Partial<Record<"key", "a" | 1 | true>>;
-    };
-  };
+	type ExpectedExtractRouteData = {
+		string_param: {
+			path: "/:param";
+			params: Record<"param", string>;
+			query: never;
+		};
+		number_param: {
+			path: "/:param";
+			params: Record<"param", number>;
+			query: never;
+		};
+		boolean_param: {
+			path: "/:param";
+			params: Record<"param", boolean>;
+			query: never;
+		};
+		string_query: {
+			path: "/";
+			params: never;
+			query: Partial<Record<"key", string>>;
+		};
+		number_query: {
+			path: "/";
+			params: never;
+			query: Partial<Record<"key", number>>;
+		};
+		boolean_query: {
+			path: "/";
+			params: never;
+			query: Partial<Record<"key", boolean>>;
+		};
+		strings_union_param: {
+			path: "/:param";
+			params: Record<"param", "a" | "b" | "c">;
+			query: never;
+		};
+		numbers_union_param: {
+			path: "/:param";
+			params: Record<"param", 1 | 2 | 3>;
+			query: never;
+		};
+		mix_union_param: {
+			path: "/:param";
+			params: Record<"param", "a" | 1 | true>;
+			query: never;
+		};
+		strings_union_query: {
+			path: "/";
+			params: never;
+			query: Partial<Record<"key", "a" | "b" | "c">>;
+		};
+		numbers_union_query: {
+			path: "/";
+			params: never;
+			query: Partial<Record<"key", 1 | 2 | 3>>;
+		};
+		mix_union_query: {
+			path: "/";
+			params: never;
+			query: Partial<Record<"key", "a" | 1 | true>>;
+		};
+	};
 
-  assertType<
-    IsExact<
-      ExtractRouteData<typeof flat_route_config>,
-      ExpectedExtractRouteData
-    >
-  >(true);
+	assertType<
+		IsExact<
+			ExtractRouteData<typeof flat_route_config>,
+			ExpectedExtractRouteData
+		>
+	>(true);
 });
 
 Deno.test("flatten_route_config", () => {
-  const expected_flat_route_config = {
-    string_param: "/:param<string>",
-    number_param: "/:param<number>",
-    boolean_param: "/:param<boolean>",
-    string_query: "/?key<string>",
-    number_query: "/?key<number>",
-    boolean_query: "/?key<boolean>",
-    strings_union_param: "/:param<(a|b|c)>",
-    numbers_union_param: "/:param<(1|2|3)>",
-    mix_union_param: "/:param<(a|1|true)>",
-    strings_union_query: "/?key<(a|b|c)>",
-    numbers_union_query: "/?key<(1|2|3)>",
-    mix_union_query: "/?key<(a|1|true)>",
-  } as const satisfies FlatRoutes<typeof route_config>;
+	const expected_flat_route_config = {
+		string_param: "/:param<string>",
+		number_param: "/:param<number>",
+		boolean_param: "/:param<boolean>",
+		string_query: "/?key<string>",
+		number_query: "/?key<number>",
+		boolean_query: "/?key<boolean>",
+		strings_union_param: "/:param<(a|b|c)>",
+		numbers_union_param: "/:param<(1|2|3)>",
+		mix_union_param: "/:param<(a|1|true)>",
+		strings_union_query: "/?key<(a|b|c)>",
+		numbers_union_query: "/?key<(1|2|3)>",
+		mix_union_query: "/?key<(a|1|true)>",
+	} as const satisfies FlatRoutes<typeof route_config>;
 
-  assertEquals(flat_route_config, expected_flat_route_config);
+	assertEquals(flat_route_config, expected_flat_route_config);
 });
 
 Deno.test(
-  "The constraint area should be removed when creating the path",
-  () => {
-    const link = create_link_generator(flat_route_config);
-    const path = link("string_param", { param: "a" });
-    assertEquals(path, "/a");
-  },
+	"The constraint area should be removed when creating the path",
+	() => {
+		const link = create_link_generator(flat_route_config);
+		const path = link("string_param", { param: "a" });
+		assertEquals(path, "/a");
+	}
 );
